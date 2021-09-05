236e4535212aa4b9e68f902d012e741c53b4d9ac
juan <juanrm071299@gmail.com>
2021-09-05 17:03:33 -0600

    se definio el metodo run y el controladorCliente

--- Server/SocketServer.cpp
+++ Server/SocketServer.cpp
@@ -21,3 +21,59 @@ bool SocketServer::enlazarKernel() {
     listen(descriptor,4);
     return true;
 }
+
+void SocketServer::run() {
+    //validar errores al crear el socket y enlazar kernel
+    if (!crearSocket())
+        throw string("Hubo un error al crear el socket");
+
+    if (!enlazarKernel())
+        throw string("Hubo un error al enlazar el kernel");
+
+    //ciclo infinito para aceptar a los clientes
+    while(true){
+        dataSocket clientData;
+        socklen_t clientLen = sizeof(clientData.info);
+        cout << "Esperando a que conecte un cliente" << endl;
+        clientData.descriptor = accept(descriptor,(sockaddr *)&clientData.info,&clientLen); //funcion bloqueante
+
+        if (clientData.descriptor < 0){
+            cout << "Error al aceptar el cliente" << endl;
+            break;
+        }
+        else{
+            clientes.push_back(clientData.descriptor);
+            cout << "Cliente conectado" << endl;
+
+            pthread_t hilo;
+            pthread_create(&hilo,0,SocketServer::controladorCliente,(void *)&clientData);
+            pthread_detach(hilo);
+        }
+    }
+    close(descriptor);
+}
+
+void* SocketServer::controladorCliente(void* obj) {
+    dataSocket* clientData = (dataSocket * )obj;
+    while(true){
+        string mensaje;
+        char buffer[1024] = {0};
+        while(true){
+            memset(buffer,0,1024);
+            int bytes = recv(clientData->descriptor, buffer, 1024, 0); //funcion bloqueante
+            mensaje.append(buffer, bytes);
+            if (bytes <= 0 ){
+                close(clientData->descriptor);
+                pthread_exit(NULL);
+            }
+            if(bytes < 1024){
+                break;
+            }
+        }
+        cout << mensaje << endl; //mensaje del cliente
+        //mensaje es la variable que me llega del cliente y tengo que ver que hago con ella en el servidor
+        // se puede usar para el juego o para una clase
+    }
+    close(clientData->descriptor);
+    pthread_exit(NULL);
+}
--- Server/SocketServer.h
+++ Server/SocketServer.h
@@ -12,20 +12,26 @@
 #include <unistd.h>
 using namespace std;
 
+//estructura para repreesntar a los clientes
+struct dataSocket{
+    int descriptor;
+    sockaddr_in info;
+};
+
 class SocketServer{
 public:
     SocketServer();
-    void run();
+    void run(); //inicia lo que tiene que ver con el server (aceptar clientes, crear el socket, recibir mensajes, etc)
     void setMensaje(const char* msn);
 
 private:
     int descriptor; //identifica a los sistemas
     sockaddr_in info; //informacion del socket servidor
-    vector<int> clientesDescriptor; //almacena los clientes que se conecten
+    vector<int> clientes; //almacena los clientes que se conecten
 
     bool crearSocket();
     bool enlazarKernel(); // escucha clientes y define cuantos va a escuchar
-    static void* controladorCliente(void* obj);
+    static void* controladorCliente(void* obj); //lleva el control de los mensajes de los clientes, cada cliente es un hilo
 
 };
 #endif //SERVER_SOCKETSERVER_H
